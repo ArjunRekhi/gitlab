27808fdee3c3fa8da400e9744346e45927ba9f61
Your Name <you@example.com>
2019-01-16 13:03:52 +0530

    first commit

---
+++ file2.c
@@ -0,0 +1,30 @@
+#include<iostream.h>
+using namespace std;
+ class calc
+{
+	int add(int,int)
+        int sub(int,int)
+	int divide(int,int)
+	int multi(int,int)
+	 int add(int x,int y)
+	 {
+		 return x+y;
+	 }
+	int sub(int x,int y)
+	{
+		return x-y;
+	}
+	int divide(int x,int y)
+	{
+		return x/y;
+	}
+	int multi(int x,int y)
+	{
+		return x*y;
+	}
+
+	int main()
+	{
+	 int w,x,y,z;
+	 w=add(2,4);
+	}}
